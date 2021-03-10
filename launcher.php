094f7adcd3cde780f59a6d400a64a6ac341dff67
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-03-10 12:02:17 +0600

    add custom fields

--- launcher.php
+++ launcher.php
@@ -5,7 +5,11 @@
 ?>
 <?php 
 	get_header( );
-	the_post(); 
+	the_post();
+
+	$placeholder_text = get_post_meta( get_the_ID(), "placeholder", true );
+	$button_label	  = get_post_meta( get_the_ID(), "button label", true );
+	$hint			  = get_post_meta( get_the_ID(), "hint", true ); 
 ?>
 	<body>
 	<div class="fh5co-loader"></div>
@@ -36,9 +40,9 @@
 							<div class="col-lg-7 animate-box">
 								<form action="#" id="fh5co-subscribe">
 									<div class="form-group">
-										<input type="text" class="form-control" placeholder="Enter your email">
-										<input type="submit" value="Send" class="btn btn-primary">
-										<p class="tip">Please enter your email address for early access.</p>
+										<input type="text" class="form-control" placeholder="<?php echo esc_attr( $placeholder_text ); ?>">
+										<input type="submit" value="<?php echo esc_attr($button_label); ?>" class="btn btn-primary">
+										<p class="tip"><?php echo esc_html( $hint ); ?></p>
 									</div>
 								</form>
 							</div>
