da9569abc409938b5ab256e790629f5bdd4e6507
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-03-10 13:37:44 +0600

    page template on demand js and css file loaded

---
+++ testtemplate.php
@@ -0,0 +1,21 @@
+<?php
+/*
+*Template Name: Test Template
+*/
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<?php get_header( ); ?>
+</head>
+<body <?php body_class(); ?>>
+
+	<h1>
+		<?php the_title(); ?>	
+	</h1>
+	
+	<?php get_footer(); ?>
+
+</body>
+</html>
